b43a53fc47a7abf1ddaf586cd9636294e804ec9e
Ayanda Sibanda <ayanda.sibanda17@gmail.com>
2023-03-16 17:47:22 -0700

    This C program prints a string followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * return: Always 0 when the code is a success.
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
