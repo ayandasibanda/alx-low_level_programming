75d3e779794d93431cdcccda4b0f2b9d9c80c7e4
Ayanda Sibanda <ayanda.sibanda17@gmail.com>
2023-04-18 20:10:00 -0700

    Code changes

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,14 +1,34 @@
-/**
- * struct dog - a type representing a dog with a name, age, and owner
- * @name: a pointer to a character string representing the name of the dog
- * @age: a float representing the age of the dog
- * @owner: a pointer to a character string representing the name of the dog's owner
- *
- * Description: This type represents a dog with a name, age, and owner.
- */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
+#ifndef DOG_H
+#define DOG_H
+
+
+	/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 * dog_t - typedef for struct dog
+	 * Description: Longer description
+	 */
+
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+
+	#endif
+
 
