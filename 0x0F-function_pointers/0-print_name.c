4b6ce814ca356cd5ba204b418c67af7db1d61098
Ayanda Sibanda <ayanda.sibanda17@gmail.com>
2023-04-19 04:53:06 -0700

    Changes to files

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -12,8 +12,20 @@
  * and returns void.
  */
 
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
+
 
