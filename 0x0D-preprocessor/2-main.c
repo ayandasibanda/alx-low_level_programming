af130aea7831a53b0f6ae41b988310986b82412b
Ayanda Sibanda <ayanda.sibanda17@gmail.com>
2023-04-18 19:31:09 -0700

    Changes to code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,10 +4,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#include <stdio.h>
 
+int main() {
+    printf("This program was compiled from the file: %s\n", __FILE__);
+    return 0;
+}
 
